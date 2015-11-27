c950eb88415b9dba217d63a93c7e0e8a6dd9a7c5
Jebb Domingo <jebb@wizmediateam.com>
2015-11-27 13:55:28 +0800

    Re: #4 Added basic toolbar settings
    Override default “standard” toolbar when option “toolbar” is defined in
    the calling template

--- resources/assets/ckeditor/config.js
+++ resources/assets/ckeditor/config.js
@@ -7,6 +7,11 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
 	// For complete reference see:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+	
+	config.toolbar_Basic =
+	[
+		['Bold', 'Italic', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Unlink','-','About']
+	];
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
