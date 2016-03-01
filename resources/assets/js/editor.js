779fb2c2cb5f1b741504c570c02c6dcee59d00ea
Jebb Domingo <jebb@wizmediateam.com>
2016-03-02 07:28:57 +0800

    Re: #8 Updated validation with koowa
    - Hook into koowa validate event instead of overriding
    Koowa.Controller.Form

--- resources/assets/js/editor.js
+++ resources/assets/js/editor.js
@@ -8,31 +8,19 @@
 
 if(!Koowa) var Koowa = {};
 
-Koowa.Controller.Form = new Class({
+kQuery(document).ready(function(){
+    // Hook into koowa controller validate event
+    kQuery('.-koowa-form').on('koowa:validate', function(context) {
+        // Loop through all the editor intances
+        // See: http://ckeditor.com/forums/CKEditor-3.x/Getting-CKEDITOR-instance
+        for(var i in CKEDITOR.instances)
+        {
+            element = document.getElementById(CKEDITOR.instances[i].name);
 
-    Extends: Koowa.Controller,
-
-    _action_default: function(action, data, novalidate){
-        if(!novalidate && !this.fireEvent('validate')) {
-            return false;
-        }
-
-        // Don't validate if novalidate is set
-        if(!novalidate){
-            // Loop through all the editor intances
-            // See: http://ckeditor.com/forums/CKEditor-3.x/Getting-CKEDITOR-instance
-            for(var i in CKEDITOR.instances) {
-
-                element = document.getElementById(CKEDITOR.instances[i].name);
-
-                // If any instance is empty then abort the save action
-                if(!CKEDITOR.instances[i].getData() && element.classList.contains('ckeditor-required')) {
-                    return false;
-                }
+            // If any instance is empty then abort the save action
+            if(!CKEDITOR.instances[i].getData() && element.classList.contains('ckeditor-required')) {
+                return false;
             }
         }
-
-        this.form.adopt(new Element('input', {name: '_action', type: 'hidden', value: action}));
-        this.form.submit();
-    }
+    });
 });
