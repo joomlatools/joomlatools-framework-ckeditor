92601c8b693bca4dcc90cea2fb32095ca3a9892b
Jebb Domingo <jebb@timble.net>
2017-12-20 12:27:37 +0800

    #29: Replace iframedialog plugin with MagnificPopup

--- resources/assets/ckeditor/plugins/filelink/plugin.js
+++ resources/assets/ckeditor/plugins/filelink/plugin.js
@@ -1,6 +1,5 @@
 CKEDITOR.plugins.add( 'filelink', {
     icons: 'filelink',
-    requires: ['iframedialog'],
     init: function( editor ) {
         editor.ui.addButton( 'Filelink', {
             label: 'Insert File',
@@ -9,26 +8,22 @@ CKEDITOR.plugins.add( 'filelink', {
         });
 
         var cmd = editor.addCommand('filelink.cmd', {exec:showDialogPlugin});
-
-        CKEDITOR.dialog.addIframe(
-            'filelink.dlg',
-            'File Link',
-            editor.config.fileLinkUploadUrl,
-            800,
-            400,
-            function(){
-            }
-        );
     }
 });
 
 function showDialogPlugin(e){
-    e.openDialog('filelink.dlg');
+    kQuery.magnificPopup.open({
+        items: {
+            src: e.config.fileLinkUploadUrl,
+            type: "iframe"
+        },
+        mainClass: "koowa_dialog_modal"
+    });
 }
 
 kQuery(document).ready(function($) {
     CKEDITOR.on("instanceReady", function(e) {
         // Remove built-in image button
-        $('#cke_21').hide();
+        $('.cke_button__image').hide();
     });
 });
