f839b91e1611b9c387e6ed9f722f354209309bd8
Arunas Mazeika <arunas@timble.net>
2019-05-21 17:29:17 +0200

    #39 Add placeholder support

--- view/editor/html.php
+++ view/editor/html.php
@@ -30,7 +30,7 @@ class ComCkeditorViewEditorHtml extends ComKoowaViewHtml
                 'height'               => '',
                 'width'                => '',
                 'extraAllowedContent'  => ["hr[id]","img[data-*,title](*)","a[data-*,title](*)"],
-                'extraPlugins'         => ['autocorrect'],
+                'extraPlugins'         => ['confighelper', 'autocorrect'],
                 'removePlugins'        => ['uploadfile', 'uploadimage'],
                 'removeButtons'        => ['Subscript','Superscript','Styles','Anchor','AutoCorrect','Cut','Copy','PasteText'],
             ]
@@ -76,6 +76,9 @@ class ComCkeditorViewEditorHtml extends ComKoowaViewHtml
         //Set editor class
         $class = KObjectConfig::unbox($this->getConfig()->attribs->class);
         $context->data->class = is_array($class) ? implode(' ', $class) : $class;
+
+        //Set editor placeholder
+        $context->data->placeholder = $context->data->attribs->placeholder ?: $this->getConfig()->attribs->placeholder;
 
         parent::_fetchData($context);
     }
